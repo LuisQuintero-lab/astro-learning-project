e8a4e7fb83f421d35f9495816dc0459db99cc763
Luis Quintero <quinteropartidaluis@gmail.com>
2025-08-28 00:51:10 -0600

    Astro server islands for content and vercel-ip-country request

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,24 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   env: {
     schema: {
       SHOW_BUY_BUTTON: envField.boolean({ context: 'server', access: 'public' }),
-      SCORE_API_ENDPOINT: envField.string({ context: 'client', access: 'public' }),
+      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' }),
     }
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
