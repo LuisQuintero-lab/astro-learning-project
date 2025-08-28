a9425799d9297e925551e22366c676b58cc2b773
Luis Quintero <quinteropartidaluis@gmail.com>
2025-08-27 23:55:05 -0600

    Initial commit: Astro.js learning project setup

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig, envField } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean({ context: 'server', access: 'public' }),
+      SCORE_API_ENDPOINT: envField.string({ context: 'client', access: 'public' }),
+    }
+  }
+});
